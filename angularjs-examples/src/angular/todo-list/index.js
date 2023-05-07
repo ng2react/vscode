730ec626085e4cd1e552d96c84c9f6ec59e08027
maxbilbow <maxbilbow@gmail.com>
2023-05-07 10:42:29 +0100

    ATR-4 Better wrapping of react conversion

--- angularjs-examples/src/angular/todo-list/index.js
+++ angularjs-examples/src/angular/todo-list/index.js
@@ -1,30 +1,40 @@
 import angular from "angular";
 import './todo-list.component.js'
 import './todo-list-service.js'
-import { angularize } from "../../support/angularise.cjs";
 import TodoList from "../../react/todo-list/todo-list.jsx";
+import { angularize } from "@ng2react/support";
 
 const app = angular.module('todoListApp', ['todoList', 'todoListService'])
     .component('todoListApp', {
         controller: function ($log, $scope, todoListService) {
             $scope.items = []
-    
+
             todoListService.getItems().then((items) => {
                 $scope.items = items;
                 $scope.$watch('items.length', () => {
-                    todoListService.saveList($scope.items).catch($log.error)
-                })
+                    todoListService.saveList($scope.items).catch($log.error);
+                    $scope.items = [...$scope.items]; // Force update
+                });
             });
 
+            // REACT STUFF:
+
+            // DIFF: Replace watchers with onChange callbacks
             $scope.setItems = (items) => {
-                $scope.items = items;
-            }
+                $scope.$evalAsync(() => {
+                    $scope.items = items;
+                });
+            };
         },
-        template: '<todo-list items="items"></todo-list>\n'+
-        '<todo-list-react items="items" set-items="setItems"></todo-list-react>'
+        template: '<todo-list items="items"></todo-list>\n' +
+            '<todo-list-react items="items" set-items="setItems(items)"></todo-list-react>'
     });
 
-angularize(TodoList, 'todoListReact', app, {
-    items: '<',
-    setItems: '&'
+angularize(TodoList, {
+    module: app,
+    name: 'todoListReact',
+    bindings: {
+        items: '<',
+        setItems: '&'
+    }
 });
